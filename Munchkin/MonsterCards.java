c22b61180a4f1c3bf96a153521e64602368d7b8c
jonothon.pollard <jonothon.pollard@MU-C3-19.mullen.pvt.k12.co.us>
2015-02-02 14:56:22 -0700

    initial

---
+++ Munchkin/MonsterCards.java
@@ -0,0 +1,40 @@
+
+public class MonsterCards extends DoorCards
+	{
+
+	}
+//.add(1, 1, "Lame Goblin", ,"Bad Stuff: He whacks you with his crutch. You lose a level.", 2);
+
+//.add(2, 1, "Mr.Bones", "If you must flee, you lose a level even if you escape", "Bad Stuff: His bony touch costs you 2 levels", 1);
+
+//.add(1, 1, "Maul Rat", "A creature from Hell. +3 against Clerics.", "Bad Stuff: She whack you. Lose a level.", 1);
+
+//.add(6, 1, "Pukachu", "Gain an extra level if you defeat it without using help or bonuses.", "Bad Stuff: Projective vomiting attack! Discard your whole hand.", 2);
+
+//.add(1, 1, "Drooling Slime", "Yucky slime! +4 against Elves.", "Bad Stuff: Discard the Footgear you are wearing. Lose a level if you have no Footgear.", 1);
+
+//.add(6, 1, "Platycore", "Resists magic. +6 against Wizards.", "Bad Stuff: Either discard you whole hand or lose 2 levels.", 2);
+
+//.add(16, 2,"Hippogriff", "Will not pursue anyone of level 3 or below.", "Bad Stuff: You are stomped and chomped. You drop things as you flee. Starting with the player on your right each player take one treasure card from in front of you or(without looking) from your hand.", 4);
+
+//.add(2, 1, "Large Angry Chicken", "Fried chicken is delicious. Gain an extra level if you defeat it with fire or flame.", "Bad stuff: Very painful pecking. lose a level." , 1);
+
+//.add(18, 2, "Squidzilla", "Slimy! Elves are -4! Will not pursure anyone of Level 4 or below except Elves.", "Bad Stuff: You are grabbed, slimed, crushed, and gobbled. You are dead, dead, dead. Any questions?", 4);
+
+//.add(2, 1, "Flying Frogs", "-1 to run away", "Bad Stuff: They bite! Lose 2 levels.", 1)
+
+//.add(12, 1, "BigFoot", "+3 against Dwarves and Halflings.", "Bad Stuff: Stomps you flat and eats your hat. Lose the Headgear you were wearing." 3);
+
+//.add(8, 1, "Gazebo", "No one can  help you. You must face the Gazebo alone.", "Bad Stuff: Lose 3 levels.", 2)
+
+//.add(6, 1, "Shrieking Geek", "+6 against warriors", "Bad Stuff: You become a normal, borning Human. Discard any Race or Class cards in play.", 2);
+
+//.add(4, 1, "Undead Horse", "+5 against Dwarves", "Bad Stuff: Kicks, bites, and smells aweful. Lose 2 levels.", 2)
+
+//.add(8, 1, "Face Sucker", "It's gross! +6 against Elves.", "Bad Stuff" When it sucks your face off, your Headgear goes with it. Discard the Headgear you are wearing and lose a level.", 2)
+
+//.add(16, 2, "King Tut", "Will not pursue anyone of Level 3 or below. Higher-Level characters lose 2 leves, even if they escape.", 4)
+
+//.add(14, 1, "Unspeakably Awful Indescribable Horror", "+4 against warriors.", "Bad Stuff: Unspeakably aweful death for anyone but a Wizard. A Wizard merely loses his powers - discard the Wizard card.", 4)
+
+//.add(8, 1. "Ghoulfiends", "No items or other bonuses help against them - fight with your level only." "Bad Stuff: Your level becomes equal to the Lowest LEvel of any character in play.", 2)
---
+++ Munchkin/src/MonsterConstructor.java
@@ -0,0 +1,94 @@
+
+public class MonsterConstructor
+{
+	private int monsterLevel;
+	private int lvsGivenIfDefeated;
+	private String name;
+	private String individualPerks;
+	private String badStuff;
+	private int treasuresGivenIfDefeated;
+	
+
+	public MonsterConstructor(int mL, int lGID, String n, String iP, String bS, int tGID)
+		{
+		monsterLevel = mL;
+		lvsGivenIfDefeated = lGID;
+		name = n;
+		individualPerks = iP;
+		badStuff = bS;
+		treasuresGivenIfDefeated = tGID;
+		}
+
+
+	public int getMonsterLevel()
+		{
+		return monsterLevel;
+		}
+
+
+	public void setMonsterLevel(int monsterLevel)
+		{
+		this.monsterLevel = monsterLevel;
+		}
+
+
+	public int getLvsGivenIfDefeated()
+		{
+		return lvsGivenIfDefeated;
+		}
+
+
+	public void setLvsGivenIfDefeated(int lvsGivenIfDefeated)
+		{
+		this.lvsGivenIfDefeated = lvsGivenIfDefeated;
+		}
+
+
+	public String getName()
+		{
+		return name;
+		}
+
+
+	public void setName(String name)
+		{
+		this.name = name;
+		}
+
+
+	public String getIndividualPerks()
+		{
+		return individualPerks;
+		}
+
+
+	public void setIndividualPerks(String individualPerks)
+		{
+		this.individualPerks = individualPerks;
+		}
+
+
+	public String getBadStuff()
+		{
+		return badStuff;
+		}
+
+
+	public void setBadStuff(String badStuff)
+		{
+		this.badStuff = badStuff;
+		}
+
+
+	public int getTreasuresGivenIfDefeated()
+		{
+		return treasuresGivenIfDefeated;
+		}
+
+
+	public void setTreasuresGivenIfDefeated(int treasuresGivenIfDefeated)
+		{
+		this.treasuresGivenIfDefeated = treasuresGivenIfDefeated;
+		}
+	
+}
