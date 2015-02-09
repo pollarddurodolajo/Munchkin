e06062c651bb32a7714899ffbde57ede8e920e4e
pollarddurodolajo <pollarddurodolajo@mullenmail.com>
2015-02-08 20:21:34 -0700

    fixed and added

--- Munchkin/src/TreasCards.java
+++ Munchkin/src/TreasCards.java
@@ -1,99 +1,37 @@
+import java.util.ArrayList;
+
 public class TreasCards
 {
-	private String tOTC;
-	private String name;
-	private String desc;
-	private int bonus;
-	private int hands;
-	private int bigOrSmall;
-	private int goldValue;
-	
-	
-
-	
-
-	public TreasCards(String tC, String n, String des, int bon, int han, int bOS, int gV)
+		protected String type;
+		protected String name;
+		protected int bonus;
+		private int value;
+		
+		ArrayList<TreasCards> listOfTC = new ArrayList<TreasCards>();
+		public void makeDoorCards()
 		{
-		tOTC = tC;
-		name = n;
-		desc = des;
-		bonus = bon;
-		hands = han;
-		bigOrSmall = bOS;
-		goldValue = gV;
-		}
-
-
-
-
-
-	public String gettOTC()
-		{
-		return tOTC;
-		}
-
-	public void settOTC(String tOTC)
-		{
-		this.tOTC = tOTC;
-		}
-
-	public String getName()
-		{
-		return name;
-		}
-
-	public void setName(String name)
-		{
-		this.name = name;
-		}
-
-	public String getDesc()
-		{
-		return desc;
-		}
-
-	public void setDesc(String desc)
-		{
-		this.desc = desc;
-		}
-
-	public int getBonus()
-		{
-		return bonus;
-		}
-
-	public void setBonus(int bonus)
-		{
-		this.bonus = bonus;
-		}
-
-	public int getHands()
-		{
-		return hands;
-		}
-
-	public void setHands(int hands)
-		{
-		this.hands = hands;
-		}
-
-	public int getBigOrSmall()
-		{
-		return bigOrSmall;
-		}
-
-	public void setBigOrSmall(int bigOrSmall)
-		{
-		this.bigOrSmall = bigOrSmall;
-		}
-
-	public int getGoldValue()
-		{
-		return goldValue;
-		}
-
-	public void setGoldValue(int goldValue)
-		{
-		this.goldValue = goldValue;
+		listOfTC.add(new TreasCards("Weapon", "Mace of Sharpness", "", 4, 1, 2, 600));
+		listOfTC.add(new TreasCards("Weapon", "Broad Sword", "", 3, 1, 2, 400));
+		listOfTC.add(new TreasCards("Weapon", "Huge Rock", "", 3, 2, 1, 0));
+		listOfTC.add(new TreasCards("Weapon", "11 Foot Pole", "", 1, 2, 2, 200));
+		listOfTC.add(new TreasCards("Weapon", "Hammer of Kneecapping", "", 4, 1, 2, 600));
+		listOfTC.add(new TreasCards("Weapon", "Staff of Napalam", "", 5, 1, 2, 800));
+		listOfTC.add(new TreasCards("Weapon", "Rapier of Unfairness", "", 3, 1, 2, 600));
+		listOfTC.add(new TreasCards("Weapon", "Stepladder", "", 3, 1, 1, 400));
+		listOfTC.add(new TreasCards("Weapon", "Cheese Grater of Peace", "", 3, 1, 2, 400));
+		listOfTC.add(new TreasCards("Potion", "Pollymorph Potion", "Use during any combat. Turns any one monster into a parrot, which flies away, leaving its treasure behind. Usable once only.", 0, 0, 2, 1300));
+		listOfTC.add(new TreasCards("Potion", "Flaming Poison Potion", "Use during any combat. +3 to either side. Usable once only.", 0, 0, 2, 100));
+		listOfTC.add(new TreasCards("Potion", "Cotion of Ponfusion", "Use during any combat. +3 to either side. Usable once only.", 0, 0, 2, 100));
+		listOfTC.add(new TreasCards("Armor", "Flaming Armor", "", 2, 0, 2, 400));
+		listOfTC.add(new TreasCards("Armor", "Slimy Armor", "", 1, 0, 2, 200));
+		listOfTC.add(new TreasCards("Armor", "Mithril Armor", "", 3, 0, 1, 600));
+		listOfTC.add(new TreasCards("Footgear", "Sandals Of Protection", "Curse cards which you draw when kicking down the door have no effect. (Curses cast by other players will still affect you.)", 0, 0, 2, 700));
+		listOfTC.add(new TreasCards("Headgear", "Helm Of Courage", "", 1, 0, 2, 200));
+		listOfTC.add(new TreasCards("Random Bonus", "Limburger And Anchovy Sandwich", "", 3, 0, 2, 400));
+		listOfTC.add(new TreasCards("Random Bonus", "Pantyhose of Giant Strength", "", 3, 0, 2, 600));
+		listOfTC.add(new TreasCards("Random Bonus", "Spiky Knees", "", 1, 0, 2, 200));
+		listOfTC.add(new TreasCards("Random Bonus", "Hireling", "Allows you to carry and use one extra item, even if you could not otherwise. If you lose your Hireling, the item goes too. You may discard your Hireling for an automatic escape from any monster.", 1, 0, 2, 0));
+		listOfTC.add(new TreasCards("GUAL", "Go Up A Level", "You can't use this if you are currently the highest-Level player, or tied for highest.", 0, 0, 2, 0));
+		
 		}
 }
